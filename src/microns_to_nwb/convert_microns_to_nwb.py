3de0b744cdad9074ecf70789f46af62e1fe74315
weiglszonja <wszonja@gmail.com>
2022-11-29 20:43:29 +0100

    add stimulus

--- src/microns_to_nwb/convert_microns_to_nwb.py
+++ src/microns_to_nwb/convert_microns_to_nwb.py
@@ -2,11 +2,12 @@ from tools.behavior import add_eye_tracking, add_treadmill
 from tools.intervals import add_trials
 from tools.nwb_helpers import start_nwb
 from tools.ophys import add_ophys
+from tools.stimulus import add_stimulus
 
 
 def build_nwb(scan_key):
     nwb = start_nwb(scan_key)
-    # add_stimulus(scan_key, nwb)
+    add_stimulus(scan_key, nwb)
     add_eye_tracking(scan_key, nwb)
     add_treadmill(scan_key, nwb)
     add_trials(scan_key, nwb)
