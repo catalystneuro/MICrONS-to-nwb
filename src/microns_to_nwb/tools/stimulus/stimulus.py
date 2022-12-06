d1fea64953ef6900b729c096adee715e9c6cfc96
weiglszonja <wszonja@gmail.com>
2022-12-06 10:08:22 +0100

    fix field_of_view for stimulus movie

--- src/microns_to_nwb/tools/stimulus/stimulus.py
+++ src/microns_to_nwb/tools/stimulus/stimulus.py
@@ -7,9 +7,6 @@ from pynwb.image import OpticalSeries
 def add_stimulus(scan_key, nwb):
     # The stimulus images were synchronized with field 1 frame times
     timestamps = (nda.FrameTimes() & scan_key).fetch1("frame_times")
-    field_of_view = (nda.Field() & scan_key).fetch("um_height", "um_width", as_dict=True)[0]
-    width_in_meters = field_of_view["um_width"] / 1e6
-    height_in_meters = field_of_view["um_height"] / 1e6
     movie = (nda.Stimulus & scan_key).fetch1("movie")
     # The movie dimensions are (height, width, number of frames), for NWB it should be
     # transposed to (number of frames, width, height)
@@ -18,7 +15,7 @@ def add_stimulus(scan_key, nwb):
         name="visual stimulus",
         description="The visual stimulus is composed of natural movies ~30 fps in grayscale format.",
         distance=np.nan,  # unknown
-        field_of_view=[width_in_meters, height_in_meters],
+        field_of_view=np.nan,
         orientation="0 is up",
         data=H5DataIO(movie_transposed, compression=True),
         timestamps=H5DataIO(timestamps, compression=True),
