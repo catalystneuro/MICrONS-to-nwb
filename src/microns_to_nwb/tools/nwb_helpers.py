4c43ecf18c16c103db34fcaf0ee981dfee9dc8bd
weiglszonja <wszonja@gmail.com>
2023-01-25 13:57:58 +0100

    black

--- src/microns_to_nwb/tools/nwb_helpers.py
+++ src/microns_to_nwb/tools/nwb_helpers.py
@@ -18,7 +18,7 @@ def start_nwb(scan_key):
         session_description="Contains calcium imaging recorded from multiple cortical visual areas "
         "and behavioral measurements while a mouse viewed natural movies and parametric stimuli.",
         subject=Subject(subject_id="17797", species="Mus musculus", age="P75D/P81D", sex="M"),
-        session_start_time=datetime(2018, 3, int(scan_key['session'])),
+        session_start_time=datetime(2018, 3, int(scan_key["session"])),
         session_id=f"{scan_key['session']}_{scan_key['scan_idx']}",
         related_publications="https://doi.org/10.1101/2021.07.28.454025",
     )
