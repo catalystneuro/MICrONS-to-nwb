00c80e93d9a43fb4617a6ae137b17d990ebaab34
weiglszonja <wszonja@gmail.com>
2023-01-26 15:01:20 +0100

    change token access to environment variable

--- src/microns_to_nwb/tools/cave_client/cave.py
+++ src/microns_to_nwb/tools/cave_client/cave.py
@@ -1,40 +1,31 @@
-import json
-from pathlib import Path
+import os
 
 from caveclient import CAVEclient
 from caveclient.base import AuthException
 
 
-def get_client(token):
-
+def get_client():
     try:
         client = CAVEclient("minnie65_public_v343")
     except AuthException:
+        # Initialize client without datastack name
         client = CAVEclient()
+        # Access token from environment
+        assert "CAVE" in os.environ, (
+            "The token for CAVE could not be read from the environment."
+            "Please set the environment variable named CAVE with the token."
+        )
+        token = os.environ["CAVE"]
+        # Save token at default location used by caveclient
         client.auth.save_token(token=token, overwrite=True)
 
+        # Retry access datastack
         client = CAVEclient("minnie65_public_v343")
     return client
 
 
-def get_token_from_external_file(external_file_path):
-    """
-    Returns the CAVEclient token saved in an external file in JSON format. (e.g. "caveclient_token.json")
-    The external file is expected to have a 'token' field with the secret token string.
-    For first usage, follow the instructions at https://github.com/seung-lab/CAVEclient/blob/master/CAVEclientExamples.ipynb
-    to generate a new token and save it in an external file.
-    """
-    assert Path(external_file_path).is_file(), f"{external_file_path} does not exist! Please create it."
-    with open(external_file_path) as f:
-        external_file = json.load(f)
-        assert "token" in external_file, f"'token' field is missing from {external_file_path}."
-
-    return external_file["token"]
-
-
-def get_functional_coreg_table(scan_key, token_file_path):
-    token = get_token_from_external_file(token_file_path)
-    client = get_client(token=token)
+def get_functional_coreg_table(scan_key):
+    client = get_client()
 
     coreg_table = client.materialize.query_table(
         table="functional_coreg",
