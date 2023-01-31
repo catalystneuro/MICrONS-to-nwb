bac298b55ccf3ce4ea79be44cd2d031df7312fef
weiglszonja <wszonja@gmail.com>
2023-01-31 14:14:51 +0100

    add inspector and dandi upload to convert_session method

--- src/microns_to_nwb/convert_session.py
+++ src/microns_to_nwb/convert_session.py
@@ -2,8 +2,11 @@ from concurrent.futures import as_completed, ProcessPoolExecutor
 from pathlib import Path
 from warnings import warn
 
-import datajoint as dj
+from neuroconv.tools.data_transfers import automatic_dandi_upload
+from nwbinspector import inspect_nwb
+from nwbinspector.inspector_tools import format_messages, save_report
 from tqdm import tqdm
+import datajoint as dj
 
 dj.config["database.host"] = "tutorial-db.datajoint.io"
 dj.config["database.user"] = "microns"
@@ -19,7 +22,7 @@ from tools.stimulus import get_stimulus_movie_timestamps
 
 def convert_session(
     ophys_file_path: str,
-    stimulus_movie_file_paths: list,
+    stimulus_movie_file_path: str,
     stimulus_movie_timestamps_file_path: str,
     nwbfile_path: str,
     verbose: bool = True,
@@ -33,7 +36,7 @@ def convert_session(
 
     source_data = dict(
         Ophys=dict(file_path=ophys_file_path, scan_key=scan_key),
-        Video=dict(file_paths=stimulus_movie_file_paths),
+        Video=dict(file_paths=[stimulus_movie_file_path]),
     )
 
     # Shifting times to earliest provided behavioral timestamp when necessary
@@ -80,7 +83,28 @@ def convert_session(
         if verbose:
             print("Conversion successful.")
 
-        return ophys_file_path
+        nwbfile_path = Path(nwbfile_path)
+        # Run inspection for nwbfile
+        results = list(inspect_nwb(nwbfile_path=nwbfile_path))
+        report_path = nwbfile_path.parent / f"{nwbfile_path.stem}_report.txt"
+        save_report(
+            report_file_path=report_path,
+            formatted_messages=format_messages(
+                results,
+                levels=["importance", "file_path"],
+            ),
+        )
+        # Upload nwbfile to DANDI
+        automatic_dandi_upload(
+            dandiset_id="000402",
+            nwb_folder_path=nwbfile_path.parent,
+            cleanup=False,
+        )
+
+        if verbose:
+            print("Cleaning up after successful upload to DANDI ...")
+        Path(ophys_file_path).unlink()
+        Path(stimulus_movie_file_path).unlink()
 
     except Exception as e:
         warn(f"There was an error during conversion. The source files are not removed. The full traceback: {e}")
@@ -107,16 +131,11 @@ def parallel_convert_sessions(
                         convert_session,
                         nwbfile_path=nwbfile_path,
                         ophys_file_path=str(ophys_file_path),
-                        stimulus_movie_file_paths=[stimulus_movie_file_path],
+                        stimulus_movie_file_path=stimulus_movie_file_path,
                         stimulus_movie_timestamps_file_path=stimulus_movie_timestamps_file_path,
                         verbose=False,
                     )
                 )
             for future in as_completed(futures):
-                source_file_path = future.result()
+                future.result()
                 progress_bar.update(1)
-                if source_file_path:
-                    try:
-                        Path(source_file_path).unlink()
-                    except PermissionError:  # pragma: no cover
-                        warn(f"Unable to clean up source file {source_file_path}! Please manually delete them.")
